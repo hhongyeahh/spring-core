3a3582f6dd93bf1c70c26d836d199ded19841eb2
hhongyeahh <hhongyeahh@users.noreply.github.com>
2024-05-07 14:14:15 +0900

    spring-basic until appConfig 리팩토링

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,7 +2,9 @@ package hello.core;
 
 import hello.core.Order.OrderService;
 import hello.core.Order.OrderServiceImpl;
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -10,11 +12,30 @@ import hello.core.member.MemoryMemberRepository;
 public class AppConfig {
     //애플리케이션의 전체 동작 방식을 구성(config)하기 위해서, 구현 객체를 생성하고,
     // 연결하는 책임을 가지는 별도의 설정 클래스를 만들자
-    public MemberService memberService(){
-        return new MemberServiceImpl(new MemoryMemberRepository());//구현 객체 생성
+
+    //AppConfig 리팩토링 전
+//    public MemberService memberService(){
+//        return new MemberServiceImpl(new MemoryMemberRepository());//구현 객체 생성
+//    }
+//
+//    public OrderService orderService(){
+//        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());//구현 객체 생성
+//    }
+
+    //AppConfig 리팩토링 후
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService(){
-        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());//구현 객체 생성
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy(){
+        return new FixDiscountPolicy();
     }
 }
