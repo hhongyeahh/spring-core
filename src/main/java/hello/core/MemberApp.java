1395e0221ed1530cdae3e89b8e0402a835b95e91
hhongyeahh <hhongyeahh@users.noreply.github.com>
2024-05-09 01:30:25 +0900

    spring-basic from 스프링 컨데이너 생성 to 스프링 빈 조회 - 기본

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,16 +4,23 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
 
-        //DIP 위반
+//        DIP 위반
 //        MemberService memberService = new MemberServiceImpl();
 
-        //해결책 -> appconfig 구현 객체 생성 후, 생성자로 의존관계 주입
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        해결책 -> appConfig 구현 객체 생성 후, 생성자로 의존관계 주입
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+//        MemberApp 에 스프링 컨테이너 적용
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService",MemberService.class);
+
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,18 +7,25 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args){
 
-        //DIP 위반
+//        DIP 위반
 //        MemberService memberService = new MemberServiceImpl();
 //        OrderService orderService = new OrderServiceImpl();
 
-        //해결책 -> appconfig 구현 객체 생성 후, 생성자로 의존관계 주입
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        해결책 -> appconfig 구현 객체 생성 후, 생성자로 의존관계 주입
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+//        OrderApp 에 스프링 컨테이너 적용
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
 
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
