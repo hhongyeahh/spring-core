da13d52cd373b7420bb40e0e1ba1172f73e9d824
hhongyeahh <hhongyeahh@users.noreply.github.com>
2024-05-10 00:31:40 +0900

    spring-basic from 스프링 빈 조회 - 동일한 타입이 둘 이상 to 스프링 빈 설정 메타 정보-BeanDefinition

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -22,28 +22,10 @@ public class AppConfig {
 //        return new MemberServiceImpl(new MemoryMemberRepository());//구현 객체 생성
 //    }
 //
-//    public OrderService orderService(){t.annotation.Configuration;
-//
-//@Configuration
-//public class AppConfig {
-//    //애플리케이션의 전체 동작 방식을 구성(config)하기 위해서, 구현 객체를 생성하고,
-//    // 연결하는 책임을 가지는 별도의 설정 클래스를 만들자
-//
-//    //AppConfig 리팩토링 전
-////    public MemberService memberService(){
-////        return new MemberServiceImpl(new MemoryMemberRepository());//구현 객체 생성
-////    }
-////
-////    public OrderService orderService(){
-////        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());//구현 객체 생성
-////    }
-//
-//    //AppConfig 리팩토링 후
-//    // -> 역할과 구현 클래스가 한 눈에 들어옴
-//    // MemoryMemberRepository의 중복을 제거하여, 이제 MemoryMemberRepository를 다른 구현체로 변경할 때, 한 부분만 변경하면 됨
-//    @Bean //(name ="mmm") -> 이렇게 이름을 임의로 바꿀 수 있음
+//    public OrderService orderService(){
 //        return new OrderServiceImpl(new MemoryMemberRepository(),new FixDiscountPolicy());//구현 객체 생성
 //    }
+
 
     //AppConfig 리팩토링 후
     // -> 역할과 구현 클래스가 한 눈에 들어옴
