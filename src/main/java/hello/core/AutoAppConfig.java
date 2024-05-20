31994f927a9334e4aa4981d84be74fe7fac26f99
hhongyeahh <hhongyeahh@users.noreply.github.com>
2024-05-20 23:26:45 +0900

    spring-basic - 컴포넌트 스캔

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -4,8 +4,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+@ComponentScan(
+        basePackages = "hello.core",
+        //basePackages = {"hello.core","hello.service"},
+        //basePackageClasses = AutoAppConfig.class, -> 해당 클래스의 패키지인 hello.core
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
 
 
