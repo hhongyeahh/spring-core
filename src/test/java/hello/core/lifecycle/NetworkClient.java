4da8adb9bc8fdd1f0b851683e564ca167dd6e5c1
hhongyeahh <hhongyeahh@users.noreply.github.com>
2024-06-26 09:51:09 +0900

    spring-basic - 빈 생명주기

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,6 +1,11 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient  {
     private String url;
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
@@ -25,5 +30,17 @@ public class NetworkClient {
     //서비스 종료시 호출
     public void disconnect(){
         System.out.println("close: "+ url);
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        connect();
+        call("초기화 연결 메시지");
+
+    }
+
+    @PreDestroy
+    public void close() throws Exception {
+        disconnect();
     }
 }
