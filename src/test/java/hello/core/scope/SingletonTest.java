bb0c59c4b49ff45a977d41f42748773ae09a45b6
hhongyeahh <hhongyeahh@users.noreply.github.com>
2024-06-26 11:48:13 +0900

    spring-basic - 빈 스코프

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,39 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import java.lang.annotation.Annotation;
+
+public class SingletonTest {
+
+    @Test
+    public void singletonBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);//컴포넌트클래스를 넣어주면 자동으로 컴포넌트스캔이 되어서 등록이 됨
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        Assertions.assertThat(singletonBean1).isSameAs(singletonBean2);
+        ac.close();
+
+    }
+
+    @Scope("singleton")
+    static class SingletonBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("SingletonBean.destroy");
+        }
+
+    }
+}
